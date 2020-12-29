6fdd1a890a21f5241f0a0a2c41cd37fdf2bf44fd
swara-2807 <75082712+swara-2807@users.noreply.github.com>
2020-12-29 08:58:57 -0800

    Update sketch.js

--- CGSO PROJECT/p5.play-boilerplate-master/sketch.js
+++ CGSO PROJECT/p5.play-boilerplate-master/sketch.js
@@ -6,6 +6,8 @@ function setup() {
   createCanvas(1600,400);
 
    speed = random(55,90);
+   weight = random(400,1500);
+  
   car = createSprite(50,200,50,50);
    car.velocityX = speed ;
    wall = createSprite(1500,200,60,height/2);
